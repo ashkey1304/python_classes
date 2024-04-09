baf30ffe7aa6c16bde856fb10e35ad201c083d72
Stevo Sbastian <stevosbastian.bhilai.com>
2024-04-09 08:23:59 +0530

    new

--- assignment/conditional&loops.py/Q15.py
+++ assignment/conditional&loops.py/Q15.py
@@ -1,2 +1,23 @@
 # Write a Python program to check the validity of passwords input by users.
+import re
+
+
 password_input = input("Enter the password: ")
+x = True
+while x:
+    if (len(password_input) < 6 or len(password_input) > 12):
+        break
+    elif not re.search("[a-z]", password_input):
+        break
+    elif not re.search ("[0-9]", password_input):
+        break
+    elif not re.search("[A-Z]", password_input):
+        break
+    elif not re.reach("[!@#$%^&*()_+]", password_input):
+        break
+    else:
+        print("Valid Passward")
+        x = False
+        break
+    if x:
+        print("Not a Valid Passward")
